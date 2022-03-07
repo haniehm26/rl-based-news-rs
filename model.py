e3815acd7d10c5ec22171062d4730889d26390ab
haniehm26 <haniehmahdavi26@gmail.com>
2022-03-07 18:49:15 +0330

    phase II model added

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 import torch.optim as optim
 import random
@@ -9,65 +8,58 @@ from src.load_dataset import load_featured_dataset
 
 
 TARGET_UPDATE = 5
-N_ITER = 500
+N_ITER = 10
 
 
 """load datasets"""
 B, N = load_featured_dataset()
 
 
-"""create env, agent & memory"""
+"""create env, agent, memory & networks"""
 env = Environment(N, B)
 agent = Agent(env.get_action_space())
 memory = ReplayMemory()
-
-
 policy_net = agent.policy_net
 target_net = DQN().to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 optimizer = optim.RMSprop(policy_net.parameters())
 
-
-l_users = ["U687515", "U192112", "U629430", "U449564", "U24161", "U79744", "U219005"]
-l_response = ["yes", "no"]
-
+print(env.get_action_space())
 
 """specify user"""
-user_id = input("Enter your ID: ")
+# user_id = input("Enter your ID: ")
+user_id = "U687515"
+responses = ["yes", "no"]
 
 
-def run():
+def run_model():
     for iter in range(N_ITER):
         state = env.get_state(user_id)
-        state_to_action = torch.from_numpy(state)
-        action, prediction = agent.act(state_to_action.float())
-        news_id = env.cat_based_news(action)
-        title, abstract = env.get_news_info(news_id)
-        print("News Title is:", title)
-        print("News Abstract is:", abstract)
+        action_category, action_tensor = agent.act(state)
+        action_news_id = env.get_action_news_id(action_category)
+        title = env.get_news_info(action_news_id)
         # user_response = input("Do you like the news?(yes/no) ")
-        user_response = random.choice(l_response)
+        user_response = random.choice(responses)
         reward = env.get_reward(user_response)
         next_state = env.update_state(
-            current_state=state, action=news_id, reward=reward, user_id=user_id
+            current_state=state, action=action_news_id, reward=reward, user_id=user_id
         )
-        print("State is:", np.mean(state))
-        print("Action is:", action)
-        print("Reward is:", reward)
-        print("Next State is:", np.mean(next_state))
-        next_state = torch.from_numpy(np.vstack(next_state).astype(np.float))
-        reward = torch.tensor([reward], device=device)
         memory.push(
-            state_to_action.float(), prediction, reward, next_state.float()
+            state, action_tensor, reward, next_state
         )
-        print(memory.__len__())
+        print("News Title is:", title)
+        print("State is:", torch.mean(state).item())
+        print("Action is:", action_category)
+        print("Reward is:", reward[0].item())
+        print("Next State is:", torch.mean(next_state).item())
+        print("Memory Size is:", memory.__len__())
         optimize_model(memory, policy_net, target_net, optimizer)
         if iter % TARGET_UPDATE == 0:
             target_net.load_state_dict(policy_net.state_dict())
-        print("ITER", iter)
+        print("ITER", iter, "\n")
     print("---DONE---")
 
 
 if __name__ == "__main__":
-    run()
+    run_model()
