39cd86f2700c06199e449f951d4b6e70b63b06bb
Hanieh Mahdavi <haniehmahdavi26@gmail.com>
2022-04-03 19:14:49 +0430

    add Abrarvan domain

--- front/code.js
+++ front/code.js
@@ -41,7 +41,7 @@ function submit_response(event) {
 
 function recommend_news(user_id) {
     // let api_url = `https://185.220.224.94:8000/recommend-news/${user_id}`;
-    let api_url = `https://127.0.0.1:8000/recommend-news/${user_id}`;
+    let api_url = `http://185.220.224.72:8000/recommend-news/${user_id}`;
     // fetch url and make a get request
     fetch(api_url)
         .then(
@@ -63,7 +63,7 @@ function recommend_news(user_id) {
 
 function get_user_response(user_response) {
     // let api_url = `https://185.220.224.94:8000/response/${user_response}`;
-    let api_url = `https://127.0.0.1:8000/response/${user_response}`;
+    let api_url = `http://185.220.224.72:8000/response/${user_response}`;
     // fetch url and make a get request
     fetch(api_url)
         .then(
